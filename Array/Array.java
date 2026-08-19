d27e0d35f2e7226a328eef7c31f872e1ab19e6ad
Cryptopranali <pranaliwadtile@gmail.com>
2026-08-20 00:09:51 +0530

    Create Array.java

---
+++ Array/Array.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Array {
+    public static void main(String[] args) {
+       int marks[] = new int[3];
+       marks[0] = 97;
+       marks[1] = 87;
+       marks[2] = 67;
+       
+       for(int i= 0; i<3 ; i++){
+        System.out.println(marks[i]);
+       }
+    }
+}
